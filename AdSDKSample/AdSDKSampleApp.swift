d58e8f010a2eddec53d292bb75b03067313aed5b
MD RUBEL <hello@mdrubel.net>
2025-05-11 22:58:25 +0600

    Initial Commit

---
+++ AdSDKSample/AdSDKSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdSDKSampleApp.swift
+//  AdSDKSample
+//
+//  Created by MD RUBEL on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AdSDKSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdSDKSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdSDKSample
+//
+//  Created by MD RUBEL on 5/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
